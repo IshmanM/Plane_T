743b51dd78134c92f261f6a154294db224b6ede0
IshmanM <ishman.mann7@gmail.com>
2026-06-04 14:33:35 -0400

    more tuning, and added filters

--- detection.py
+++ detection.py
@@ -21,8 +21,11 @@ class Measurement:
 
 
 
-LOWER_HSV = np.array([14, 50, 100])
-UPPER_HSV = np.array([25, 130, 200])
+# LOWER_HSV = np.array([14, 50, 100])
+# UPPER_HSV = np.array([25, 130, 200])
+LOWER_HSV = np.array([13, 35, 70])
+UPPER_HSV = np.array([28, 190, 255])
+MIN_CONTOUR_AREA = 100
 
 # approximate the focal length
 px_focal_length = 500  # = average of a few (reference_pixel_width * reference_distance / reference_width)
@@ -30,9 +33,9 @@ w =  0.065 # real marker width
 
 def detector(frame):
     
-    #implementation TBD#################3
+    #implementation TBD#################
     # need to account for case where nothing detected...
-    # need to throw in some extra blur/filtering/rejection here, before even fedd to kalman later
+    # need to throw in some extra blur/filtering/rejection here, before even fedd to kalman later...
 
     # Classical CV Detector using thresholding
     u, v, px_w, px_h = None, None, None, None
@@ -40,18 +43,25 @@ def detector(frame):
 
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(frame, LOWER_HSV, UPPER_HSV)
+    mask = cv2.inRange(frame, LOWER_HSV, UPPER_HSV) # Color threshold
+    mask = cv2.medianBlur(mask, 5) # apply blur
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # remove small random white specks
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel) # fil small black holes/gaps 
+
     contours, heirarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) > 0: 
         largest_contour = max(contours, key = cv2.contourArea)
-        u, v, px_w, px_h = cv2.boundingRect(largest_contour)
-        u = u + px_w/2
-        v = v + px_h/2
+        area = cv2.contourArea(largest_contour)
+        if area > MIN_CONTOUR_AREA:
+            u, v, px_w, px_h = cv2.boundingRect(largest_contour)
+            u = u + px_w/2
+            v = v + px_h/2
 
-        # Depth estimator based on emperical calibration & Pose Converter
-        z = px_focal_length*w/px_w
-        x = (u - config.FRAME_W/2)*z/px_focal_length 
-        y = (v - config.FRAME_H/2)*z/px_focal_length 
+            # Depth estimator based on emperical calibration & Pose Converter
+            z = px_focal_length*w/px_w
+            x = (u - config.FRAME_W/2)*z/px_focal_length 
+            y = (v - config.FRAME_H/2)*z/px_focal_length 
 
     detection = Detection(u, v, px_w, px_h)
     measurement = Measurement(x,y,z,pitch,roll,yaw) 
--- main.py
+++ main.py
@@ -16,18 +16,20 @@ if __name__ == "__main__":
         ret, frame = cap.read()
 
         detection, measurement = det.detector(frame)
-        
+        label = "No detections"
+
         if detection.u != None:
             cv2.rectangle(frame, 
                         (int(detection.u - detection.px_w/2), int(detection.v - detection.px_h/2)),
                         (int(detection.u + detection.px_w/2), int(detection.v + detection.px_h/2)),
                         color=(0,255,0), thickness=2)
             
-            frame = cv2.flip(frame, 1) # optional flip as a last step before labelling, for viewing only 
             label = "Measurement: (x: " + f"{measurement.x:.4f}" + ", y: " + f"{measurement.y:.4f}"  + ", z: " + f"{measurement.z:.4f}" + ")"
-            cv2.putText(frame, label,
-                        (10,10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, color=(0,255,0), thickness=2)
+
+        frame = cv2.flip(frame, 1) # optional flip as a last step before labelling, for viewing only 
+        cv2.putText(frame, label,
+                    (10,10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, color=(0,255,0), thickness=2)
 
         
         cv2.imshow("Webcam Feed", frame)
